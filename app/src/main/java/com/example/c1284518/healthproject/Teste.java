355a128ed3321199e199f5eb44e98b7597c7753c
ricsgobbe <sgobbericardo@yahoo.com.br>
2015-12-11 11:25:38 -0200

    Teste J

--- app/src/main/java/com/example/c1284518/healthproject/Teste.java
+++ app/src/main/java/com/example/c1284518/healthproject/Teste.java
@@ -4,4 +4,9 @@ package com.example.c1284518.healthproject;
  * Created by c1284518 on 11/12/2015.
  */
 public class Teste {
+    String jose;
+
+    public Teste(String jose){
+        this.jose = jose;
+    }
 }
